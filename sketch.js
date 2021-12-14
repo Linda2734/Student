32230c52301d62970f9b129cccacaf9d0183592b
Linda <94357533+Linda2734@users.noreply.github.com>
2021-12-15 00:01:43 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var Linda;
+var Students = [];
+
+function setup() {
+  createCanvas(400,400);
+  Linda = new Student("Linda",11,5,"Moiza");
+
+  console.log(Linda); 
+
+  Linda.show();
+  Juha.show()
+}
+
+function draw() 
+{
+  background(30);
+
+  
+  
+}
---
+++ student.js
@@ -0,0 +1,23 @@
+
+class Student
+{
+ constructor(name,age,grade,teacher){
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+    this.teacher = teacher;
+ } 
+ 
+ show(){
+     console.log("My name is "+this.name+" and my age is "+this.age+" and I am in grade "+this.grade+" and my teacher is "+this.teacher);
+     
+    //  console.log("My name is");
+    //  console.log(this.name);
+ }
+
+ 
+ 
+}
+
+
+
